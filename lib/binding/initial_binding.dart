236d9f6afea31873e138144ddc79af0639d750db
Sadia Bennthe <tanzibamouri00@gmail.com>
2024-08-27 10:01:02 +0600

    fix: comntroller issue

--- lib/binding/initial_binding.dart
+++ lib/binding/initial_binding.dart
@@ -5,19 +5,16 @@ import 'package:upai/presentation/HomeScreen/controller/home_screen_controller.d
 import 'package:upai/presentation/Inbox/controller/inbox_screen_controller.dart';
 import 'package:upai/presentation/LoginScreen/controller/login_screen_controller.dart';
 import 'package:upai/presentation/SplashScreen/controller/splash_screen_controller.dart';
-import 'package:upai/presentation/seller-service/seller_profile_controller.dart';
 
 import '../presentation/notification/controller/notification_controller.dart';
 
 class RootBinding implements Bindings {
   @override
   void dependencies() {
-    Get.put(NetworkController());
+    Get.lazyPut(() => NetworkController(),);
     Get.put(SplashScreenController());
     Get.put(LoginController());
-    Get.put(ChatScreenController());
-    Get.put(InboxScreenController());
-    Get.put(NotificationController());
+
 
 
   }
